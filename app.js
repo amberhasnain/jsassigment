24a049f5dbe3b60aaaa79355bafe5df12609bda9
amberhasnain <134287020+amberhasnain@users.noreply.github.com>
2023-09-24 18:32:46 +0500

    work

---
+++ app.js
@@ -0,0 +1,655 @@
+///////////////////////////Chapter 1 ///////////////////////////
+                             //1// 
+/*
+var firstName="Amber"
+var lastName="Hasnain";
+var email="s.amberhz98@gmail.com";
+var phoneNumber="11222234444";
+var pwd="*************";
+
+alert(firstName);
+alert(lastName);
+alert(email);
+alert(phoneNumber);*/
+  
+                               //2//
+
+  //var message="You're learning JavaScript!";
+  //alert(message);
+  
+                                //3//
+ 
+   //var message="karachi is the save city jo ky hai hi nai :) ";
+   //alert(message);
+
+ ////////////////////////////Chapter 2///////////////////////////
+                                //1//
+
+//    var camelCase="123";
+ 
+                                 //2//
+
+/*
+var x=5;
+var y=5;
+
+*/
+
+                                //3//
+/*
+var teamName="The Best Leagues";
+alert(teamName);*/
+ 
+                                //4//
+/*
+var bestMan = "Charlie";
+var newString="Tom";
+*/
+
+///////////////////////////Chapter 3///////////////////////////////////////////////////
+                             //1//
+//var caseQty;
+                            //2//
+//var caseQty= 144;
+
+                           //3//
+
+//var myNumber = "9";
+//myNumber = Number(myNumber) + 3;
+
+                             //4//
+/*
+
+var  num1=10;
+var num2=21;
+var sum=num1 - num2;
+document.write(sum);
+*/
+
+
+                                //5//
+
+
+//var merchTotal = 100;
+//var shippingCharge = 10;
+//var orderTotal = merchTotal + shippingCharge;
+//document.write(orderTotal);
+
+                                  //6//
+
+/*
+var num1=567;
+var num2=456;
+var add=num1+num2;
+document.write(add);
+*/
+
+///////////////////////////////Chapter 4///////////////////////////////////////////
+                                  //1//
+
+//var productCost = 3.45;
+
+                                 //2//
+
+//var nameOfBand;
+
+                                 //3//
+
+//var topScore = 100;
+
+                                 //4//
+//var amberHasnain;
+ 
+                                //5//
+/*
+//legel//
+
+var firstName;
+var last_Name;
+var firstName145;
+
+//Illegel//
+
+var 224firstname;
+var first name;
+var first Name$;*/
+
+////////////////////////////////////////Chapter 5////////////////////////////////////
+
+                                           //1//
+//modulus
+
+                                           //2//
+
+//var num = (20 % 6);
+//console.log(num);
+
+                                          //3//
+
+/*
+var largeNum=1000*2000;
+console.log(largeNum);*/
+
+                                          //4//
+
+//var number=567-55;
+
+//document.write(number);
+
+
+                                           //5/
+//var leftOver = 10 % 3;
+//console.log(leftOver);
+
+                                           //6//
+//var price=3;
+//var qty=5;
+//alert(price * qty);
+
+
+/////////////////////////////////Chapter 6/////////////////////////////////////////////////////////
+
+                                   //1//
+
+/*
+var x = x + 1; 
+var x= x++;
+console.log(x)
+
+*/
+
+                                   //2//
+
+//var x=100;
+//var y= x--;
+//console.log(y);
+ 
+                                 //3//
+
+// var x = 50;
+// var y = x++;
+//console.log(y)
+
+                                 //4//
+//var y = 50;
+//var z = --y;
+//console.log(z)
+
+                                //5//
+
+//var newNum = num--;
+
+                                 //6//
+
+//var newNum = num++;
+
+                                  //7//
+
+//var num = 10;
+//num++;
+//alert(num);
+
+///////////////////////////////Question 7///////////////////////////////
+
+                                  //1//
+//var calculatedNum = 2 + (2 * 6);
+//alert(calculatedNum);
+
+                                 //2//
+
+//var calculatedNum = (2 + 2) * 6;
+//alert(calculatedNum)
+
+
+                                  //3//
+
+//var calculatedNum = (2 + 2) * (4 + 2);
+//alert(calculatedNum)
+
+                                    //4//
+
+//var calculatedNum = ((2 + 2) * 4) + 2;
+//alert(calculatedNum)
+
+                                   //5//
+
+//var calculatedNum=(2 + 2) * (4 + 10);
+//document.write(calculatedNum);
+
+                                    //6//
+//var calculatedNum=2 + (2 * 4) + 10;
+//document.write(calculatedNum)
+
+                                   //7//
+
+//var calculatedNum= 4 / (2 * 4);
+//document.write(calculatedNum)
+
+
+///////////////////////Question 8/////////////////////////////////////////
+
+                         //1//
+//var num = "2" + "2";
+//document.write(num)
+
+                            //2//
+
+//var message = ("Hello ," + " Dolly");
+//document.write(message)
+
+                           //3//
+
+//alert("33" + 3);
+
+                           //4//
+
+//var message="Pakistan Zindabad";
+//alert(message)
+
+                           //5//
+
+//cost="The cost :" + 59.80;
+
+                                  //6//
+
+//var firstPart="Hello";
+//var secondPart="Dolly";
+//var show= firstPart + secondPart;
+//alert(show)
+
+
+////////////////////////////Question 9///////////////////////////////////
+                              //1//
+ 
+ //var firstName=prompt("Enter a first name");
+ //document.write(firstName);
+
+                                  //2//
+// var country=prompt("Enter a country name");
+ //document.write(country)
+
+                             //3//
+//var Name = prompt("Enter Your Name");
+
+                                //4//
+
+//var name = prompt("Enter cat name", "Chinto");
+
+
+                             //5//
+
+//var question = "Name of parrot?";
+//var defaultAnswer = "Polly";
+//var pName = prompt(question, defaultAnswer);
+
+                               //6//
+//var response = prompt("Enter your favorite color", "red");
+//alert(response);
+
+/////////////////////////////////////////Question 10/////////////////////////////////
+                                      / /1//
+//var city = "Lahore"
+//if (city == "Lahore") {
+//alert("The City OF Cleaness ")
+//}
+
+                                      //2//
+//var answer = prompt("What is z?");
+//if(x==y){
+//alert(z)
+//}
+
+
+
+                                           //3//
+
+   //  if (ZipCode === "10010") {
+    // city = "Karachi";
+  //}
+
+                           //4//
+
+//    if (pets === 1) {
+  //  pets = 2;
+   // }
+
+ 
+
+/////////////////////////////////Question 11///////////////////////////////////////
+                                  //1//
+
+//if(firstName !== nickname) {
+  //  alert(firstName)
+//}
+
+                               //2//
+//if (num1 >= num2) {
+//alert(num1)
+
+//}
+
+                                    //3//
+//if ("Mexico" !== countryOfOrigin) {
+//alert(mexico)
+
+//}
+                                      //4//
+/*
+if (1 !== 0) {
+  alert("Congrats!");
+}*/
+
+                                 //5//
+/*
+var Name= prompt("What is your first name?");
+if (Name !== "Cat") {
+  alert("No match");
+}*/
+
+////////////////////////////Question 12///////////////////////////////
+                               //1//
+/*
+if (a >= b) {
+  alert("OK");
+}
+else {
+  alert("Not OK");
+}*/
+
+                               //3//
+
+/*var a=10;
+if (a === 10) {
+  alert("a is 10");
+ }
+else {
+  alert("a is " + a);
+}*/
+
+                                  //4//
+/*
+var cityEntered = prompt("Enter a city");
+if (cityEntered === "Lahore") {
+  alert("You entered Lahore");
+}
+else {
+  alert("Why didn't you enter Lahore?");
+}*/
+
+////////////////////////////Question 13//////////////////////////////
+                              //1//
+/*if (a === b && c === d) 
+{
+  alert(a)
+}
+
+                             //2//
+if (a === b || c !== d) {
+  alert(a)
+}
+
+                             //3//
+if ((name === "Hamza" || name === "Arsalan") && age > 60) {
+  alert(name)
+}*/
+
+                            //4//
+/*
+var a = 1;
+var b = 2;
+if (a < b || a > b) {
+  alert("One is bigger than the other");
+}*/
+
+                              //5//
+/*
+var firstName = "Amber";
+var lastName = "Hasnain";
+var inputFirstName = prompt("What is your first name?");
+var inputLastName = prompt("What is your last name?");
+if (firstName === inputFirstName && lastName === inputLastName) {
+  alert("Everything matches");
+}*/
+
+
+////////////////////////////////Question 14//////////////////////////////////////////////
+                                    //1//
+/*var password = prompt("Enter your password");
+
+if (password !== "") {
+if (password.length <= 5) {
+ alert("Password must be greater than 5");
+} else {
+alert("OK");
+} } else {
+alert("Password cannot be empty");
+}*/
+
+                                        //2//
+//if (a === 1 && c === "Max") {
+//alert("OK")
+//}
+
+
+                                    //3//
+//if (a === 1 && c === "Max") {
+
+                           //4//
+
+/*
+var a = 1;
+var b = 1;
+if (a === b) {
+  if (a <= b) {
+    alert("Both are true");
+  }
+}*/
+
+
+//////////////////////////////////Question 15////////////////////////////////
+
+//1//
+
+//let myArray = [];
+
+//2//
+
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//let text = fruits.toString();
+
+//3//
+
+//var alphabet = ["h","i","j","k"];
+//alert(alphabet[2])
+
+//4//
+//var alphabet=["h","i","j","k", "l","m", "n", "o"];
+//let length = alphabet.length;
+
+//alert(length)
+
+
+//5//
+//var numbers = [1, 2];
+//numbers.push("3");
+//alert(numbers);
+
+
+/////////////////////////////////Question 16(1)/////////////////////////////
+
+
+//1//
+
+/*
+var car=["BMW","audi","carolla"];
+car.push("mehran");
+alert(car)*/
+
+//2//
+//var Alphabet=["h","i","j","k"]
+//Alphabet.pop("k");
+//alert(Alphabet)
+
+//3//
+//var Alphabet=["h","i","j","k"];
+//Alphabet.push("l","m")
+//alert(Alphabet)
+
+//////////////////////////////////////////Question 16(2)///////////////////////////////////////////////////////
+//1//
+/*
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+sizes.shift("S");
+alert(sizes)
+*/
+//2//
+/*
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+sizes.unshift("1","2","3");
+alert(sizes)*/
+
+//3//
+//var number=["1"];
+//number.unshift("0");
+//alert(number[0])
+
+//4//
+/*
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+sizes.splice(2,0,"L");
+alert(sizes)
+*/
+
+//5//
+/*
+var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+var regSizes =sizes.slice(0, 3);
+alert(regSizes)
+*/
+//6//
+//var pets = ["dog", "cat", "ox", "duck", "frog"];
+//pets.splice(1, 3, "fish", "monkey");
+//alert(pets)
+
+
+//7//
+/*
+var pets = ["dog", "cat", "ox", "duck", "frog"];
+pets.splice(1, 2);
+alert(pets)
+*/
+//8//
+/*
+var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+pets = pets.slice(3, 5);
+alert(pets)*/
+
+///////////////////////////////////////////////Question 17-20/////////////////////////////////////////////
+
+//1//
+
+//for(var i=1; i<=10;i++){
+  //document.write(i)
+//}
+
+//2//
+//for (var i = 0; i <= 11; i++) {
+//document.write(i)
+//}
+
+  //3//
+//for(var i = 0; i <= 4; i++){
+//document.write(i)
+//}
+
+//4//
+/*
+for (var i = 0; i < 100; i++) {
+document.write(i,"<br>")}*/
+
+
+//5//
+
+//for (var i = 0; i > -3; i--) {
+ // document.write(i)
+//}
+
+//6//
+
+//var arr=10;
+
+//7//
+//var matchFound = false;
+//var noMatch = true;
+
+//8//
+//for (var i = 0; i < pets.length; i++) {
+  //document.write("CRAZy");
+//}
+
+//9//
+/*
+for (var i = 0; i < 10; i++) {
+  if (i === 1) {
+    alert(i);
+    break;
+  }
+}*/
+
+//10//
+
+/*var userNames = ["Alice", "Bob", "Charlie", "David", "Eve"];
+var firstName = prompt("Enter first name");
+var isFound = false;
+for (var i = 0; i < userNames.length; i++) {
+    if (firstName === userNames[i]) {
+    isFound = true;
+    break; 
+  }
+}
+if (isFound) {
+  alert("Welcome, " + firstName + "!");
+} else {
+  alert("Please write a correct user name.");
+}*/
+
+
+
+
+
+
+
+
+
+//11//
+
+//if (matchFound === false) {
+  //alert("Match not found");
+//}
+
+
+
+
+//12//
+/*
+var firstArr = ["a", "b", "c", "d", "e", "f"];
+var secondArr = [1, 2, 3, 4, 5, 6];
+
+for (var i = 0; i < firstArr.length; i++) {
+
+  for (var j = 0; j < secondArr.length; j++) {
+  
+    var concatenated = firstArr[i] + secondArr[j];
+   document.write(concatenated + "<br>");
+  }
+}*/
+
+
+
+
+
+
+
+
+
+
